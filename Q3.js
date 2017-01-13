411b4044531633f2db826da94427e8ef2718b77a
FVI <test@examle.com>
2017-01-12 21:41:20 -0500

    complete execercises

---
+++ Q3.js
@@ -0,0 +1,9 @@
+// Question 3
+
+// Inside of console.log, write a Boolean expression that tests
+// whether two numbers are NOT equal, and whose answer evaluates to true.
+var a =0;
+var b= 1;
+if(a!=b){
+console.log("true");
+}else {console.log("false")}
---
+++ Q9.js
@@ -0,0 +1,16 @@
+// Question 9
+
+// Build a function declaration called maxOf2 that takes in two numbers and
+// returns the greater value, or returns the first value if both of them
+// are equal.
+// Then test your function inside of a console.log by calling it with two
+// different numbers and again in another console.log with two equal numbers.
+function maxOf2(a,b){
+  if(a<b){
+    return b;
+  }else {
+    return a;
+  }
+
+};
+console.log(maxOf2(2,4));
